026c22dd937296b615b06794911ec3eb8381ba73
Roberto Simonetti <robisim74@gmail.com>
2017-11-19 14:17:43 +0100

    Upgrade angular-oauth2-oidc & remove HttpModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './material.module';
 
 const sharedModules: any[] = [
     CommonModule,
     FormsModule,
-    HttpModule,
     HttpClientModule,
     MaterialModule
 ];
